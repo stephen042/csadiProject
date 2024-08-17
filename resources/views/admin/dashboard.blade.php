4f919c56eed88ec5361ed2298215918f6824bc9b
stephen042 <makuochukwu042@gmail.com>
2024-08-17 18:46:05 +0100

    update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="pagetitle">
-    <h1>Dashboard</h1>
+    <h1>Admin</h1>
     <nav>
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="/">Home</a></li>
@@ -10,5 +10,84 @@
         </ol>
     </nav>
 </div><!-- End Page Title -->
+<section class="section">
+    <div class="row">
+        <div class="col-lg-12">
 
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">All Members</h5>
+                    <div class="table-responsive table-responsive-x">
+                        <!-- Table with stripped rows -->
+                        <table class="table datatable">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Position</th>
+                                    <th scope="col">Start Date</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Brandon Jacob</td>
+                                    <td>Organizer</td>
+                                    <td>2016-05-25</td>
+                                    <td>
+                                        <div class="btn-group" role="group">
+                                            <a href="" class="btn btn-sm btn-primary m-1"><i
+                                                    class="bi bi-pencil-square"></i>
+                                                view
+                                            </a>
+                                            <button class="btn btn-sm btn-danger m-1" wire:click="deleteUser()"
+                                                wire:confirm="Are you sure you want to delete this user?">
+                                                <i class="bi bi-archive-fill"></i>
+                                                Delete
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <!-- End Table with stripped rows -->
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+<section>
+    <div class="row">
+        <div class="col-lg-4">
+            <!-- Card with an image on top -->
+            <div class="card">
+                <img src="assets/assets/img/pics/ADFINR/ANKL.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title p-2">Food and Schooling in kastina</h5>
+                    <hr class="m-0">
+                    <p class="card-text">
+                    <div class="meta">
+                        <ul>
+                            <li>
+                                </i><span class="text-primary">2016 - 2019 </span>
+                            </li>
+                            <li>Target Beneficiaries <span class="text-primary"> - Women, Children </span>
+                            </li>
+                            <li>Funded By <span class="text-primary"> -  Mrs. Tomasa Stiedemann</span>
+                            </li>
+                            <li>
+                                Location <span class="text-primary"> - Tracyborough</span>
+                            </li>
+                        </ul>
+                    </div>
+                    </p>
+                </div>
+            </div><!-- End Card with an image on top -->
+        </div>
+    </div>
+</section>
 @endsection
--- resources/views/components/admin/aside.blade.php
+++ resources/views/components/admin/aside.blade.php
@@ -11,35 +11,35 @@
 
         <li class="nav-item">
             <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
-                <i class="bi bi-menu-button-wide"></i><span>Components</span><i class="bi bi-chevron-down ms-auto"></i>
+                <i class="bi bi-menu-button-wide"></i><span>Projects</span><i class="bi bi-chevron-down ms-auto"></i>
             </a>
             <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
                 <li>
                     <a href="#">
-                        <i class="bi bi-circle"></i><span>Create Project</span>
+                        <i class="bi bi-circle"></i><span>All Projects</span>
                     </a>
                 </li>
                 <li>
                     <a href="#">
-                        <i class="bi bi-circle"></i><span>Upload Team member</span>
+                        <i class="bi bi-circle"></i><span>Create New Project</span>
                     </a>
                 </li>
             </ul>
         </li><!-- End Components Nav -->
 
         <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#users-nav" data-bs-toggle="collapse" href="#">
-                <i class="bi bi-people"></i><span>Users</span><i class="bi bi-chevron-down ms-auto"></i>
+            <a class="nav-link collapsed" data-bs-target="#team-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-people"></i><span>Team members</span><i class="bi bi-chevron-down ms-auto"></i>
             </a>
-            <ul id="users-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <ul id="team-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
                 <li>
                     <a href="#">
-                        <i class="bi bi-circle"></i><span>Create User</span>
+                        <i class="bi bi-circle"></i><span>All Team Members</span>
                     </a>
                 </li>
                 <li>
                     <a href="#">
-                        <i class="bi bi-circle"></i><span>All users</span>
+                        <i class="bi bi-circle"></i><span>Create New Team Member</span>
                     </a>
                 </li>
             </ul>
