d2938593e0b52b2d3eaf0ea99f6e26d3d75855d1
stephen042 <makuochukwu042@gmail.com>
2025-07-18 08:23:06 +0100

    update

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -15,13 +15,16 @@
 <script src="{{asset('assets/assets/js/YTPlayer.min.js')}}"></script>
 <script src="{{asset('assets/assets/js/main.js')}}"></script>
 
+{{-- livechat --}}
+<script id="chatway" async="true" src="https://cdn.chatway.app/widget.js?id=BwojkdVq7BeJ"></script>
+
 <!-- GetButton.io widget -->
 <script type="text/javascript">
     (function () {
         var options = {
             whatsapp: "{{ config('app.App_number') }}", // WhatsApp number
             call_to_action: "Message us", // Call to action
-            position: "right", // Position may be 'right' or 'left'
+            position: "left", // Position may be 'right' or 'left'
         };
         var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
         var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
